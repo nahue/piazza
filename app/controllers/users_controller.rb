d708e1241290b2e172b95504fc31cef3e5a967b5
Nahuel Chaves <nahuel.chaves@gmail.com>
2024-03-15 00:02:04 -0300

    Build the authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_authentication only: %i[new create]
+
   def new
     @user = User.new
   end
@@ -6,7 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @organization = Organization.create(members: [@user]) # TODO: Log in user...
+      @organization = Organization.create(members: [@user])
+      @app_session = @user.app_sessions.create
+      log_in(@app_session)
+
       redirect_to root_path,
                   status: :see_other,
                   flash: { success: t('.welcome', name: @user.name) }
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'users#new'
   post 'sign_up', to: 'users#create'
 
-  # Defines the root path route ("/")
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
